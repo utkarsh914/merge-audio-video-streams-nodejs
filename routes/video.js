405de9f5d1a87d9fc6d55e7afa5f75bf7c9d7ba2
Utkarsh Tiwari <utiwari914@gmail.com>
2020-06-07 23:42:40 +0530

    code cleanup, comments added

--- routes/video.js
+++ routes/video.js
@@ -1,74 +1,36 @@
-const { spawn } = require('child_process');
 const fs = require('fs');
-
 let express = require('express');
 let router  = express.Router();
-
-//fluent ffmpeg
 var ffmpeg = require('fluent-ffmpeg');
 
 router.get('/', function(req, res){
 
-
-  var pathToMovie = './public/Lamberghini.mp4';
-
   res.set('Content-Type', 'video/mp4');
-
-  //encoding the video source
-  // ffmpeg({source: pathToMovie})
-  // 	// .seekInput(10)
-  // 	.toFormat('mp4')
-  // 	.outputOptions('-movflags frag_keyframe+empty_moov')
-  // 	.on('progress', function(progress) {
-  //   	console.log(progress);
-  // 	})
-	 //  .on('error', function(err) {
-	 //    console.log('An error occurred: ' + err.message);
-	 //  })
-	 //  .on('end', function() {
-	 //    console.log('Processing finished !');
-	 //  })
-	 //  .pipe(res, { end: true })
-	 //  // .stream(outStream, { end: true })
-
-
-  // var ffmpeg = require('fluent-ffmpeg');
-  // ffmpeg(pathToMovie)
-  // 		.toFormat('mp4')
-  //     // .preset('flashvideo')
-  //     .on('error', function (err) {
-  //         console.log('an error happened: ' + err.message);
-  //         res.send(err.message);
-  //     })
-  //     .output(res, { end: true })
-  //     .run();
-
-
-
 	var file = './public/4d';
 
 	var ffmpeg = require('fluent-ffmpeg');
 	ffmpeg()
+		// input video only stream
 	  .addInput(fs.createReadStream(`${file}-video-streamable.mp4`))
-	  // .addInput(`${file}-video-streamable.mp4`)
-	  // .addInput(fs.createReadStream(`${file}-audio.aac`))
+	  // input audio only stream
 	  .addInput(`${file}-audio.aac`)
 	  .format('mp4')
+	  // required bcz mp4 needs to write header in the front after completing whole encoding
 	  .outputOptions('-movflags frag_keyframe+empty_moov')
+	  // display progress
 	  .on('progress', function(progress) {
 	    console.log(progress);
 	  })
 	  .on('error', function(err) {
 	    console.log('An error occurred: ' + err.message);
 	  })
+	  // after whole merging operaton is finished
 	  .on('end', function() {
 	    console.log('Processing finished !');
 	  })
+	  // pipe the resulting merged stream to output
 	  .pipe(res, { end: true })
-	  // .stream(outStream, { end: true })
-	  // .save(`${file}-merged.mp4`, function(stdout, stderr) {
-	  //   console.log('file has been combined succesfully');
-	  // });
+
 })
 
 module.exports = router;
