d482e3f45bee915d6db78e7765e249d323c8786f
Utkarsh Tiwari <utiwari914@gmail.com>
2020-06-08 08:43:56 +0530

    add videos and show logs

--- routes/video.js
+++ routes/video.js
@@ -5,21 +5,24 @@ var ffmpeg = require('fluent-ffmpeg');
 
 router.get('/', function(req, res){
 
+	let range = req.headers.range;
+	console.log(range)
+
   res.set('Content-Type', 'video/mp4');
 	var file = './public/4d';
 
 	var ffmpeg = require('fluent-ffmpeg');
-	ffmpeg()
+	var proc = ffmpeg()
 		// input video only stream
 	  .addInput(fs.createReadStream(`${file}-video-streamable.mp4`))
-	  // input audio only stream
+	  // input audio only file
 	  .addInput(`${file}-audio.aac`)
 	  .format('mp4')
 	  // required bcz mp4 needs to write header in the front after completing whole encoding
 	  .outputOptions('-movflags frag_keyframe+empty_moov')
 	  // display progress
 	  .on('progress', function(progress) {
-	    console.log(progress);
+	    // console.log(progress);
 	  })
 	  .on('error', function(err) {
 	    console.log('An error occurred: ' + err.message);
@@ -28,8 +31,13 @@ router.get('/', function(req, res){
 	  .on('end', function() {
 	    console.log('Processing finished !');
 	  })
+
 	  // pipe the resulting merged stream to output
-	  .pipe(res, { end: true })
+	  var ffstream = proc.pipe();
+		ffstream.on('data', function(chunk) {
+		  console.log('ffmpeg just wrote ' + chunk.length + ' bytes');
+		});
+		ffstream.pipe(res);
 
 })
 
